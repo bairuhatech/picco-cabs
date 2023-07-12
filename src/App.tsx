5df19e7a42ec050c7ab9e786e19e2827efd2f363
Thameem <thameem@bairuhatech.com>
2023-07-12 21:17:46 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react';
-import './config/styleVariables.scss';
-import FirstStep from './components/bookings/bookingForms/bookingFirstStep/firstStep';
-import Footer from './components/footer/footer';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <Fragment>
-      <FirstStep></FirstStep>
-      <Footer></Footer>
-      </Fragment>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
