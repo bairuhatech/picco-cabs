eea59e15468ac266f2a4f608b95f0b89e1f6646e
Rishad-kc <rishad@bairuhatech.com>
2023-07-13 10:20:45 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './config/styleVariables.scss';
-import Footer from './components/footer/footer';
-import FirstStep from './components/bookings/bookingForms/bookingFirstStep/firstStep';
+import React from "react";
+import logo from "./logo.svg";
+import "./config/styleVariables.scss";
+import FirstStep from "./components/bookings/bookingForms/bookingFirstStep/firstStep";
+import Footer from "./screens/footer/footer";
 
 function App() {
-  return (
-    <div className="App">
-     <FirstStep/>
-     <Footer/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<FirstStep />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
