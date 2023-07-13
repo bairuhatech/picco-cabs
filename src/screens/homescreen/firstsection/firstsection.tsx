83836df19583bce01a156758fae8a8dd997e7fab
Shebeer <shebeeradly@gmail.com>
2023-07-13 12:42:25 +0530

    footer updated

--- src/screens/homescreen/firstsection/firstsection.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Firstsection() {
-	return <div>firstsection</div>;
-}
-
-export default Firstsection;
--- src/screens/homescreen/offersection/offersection.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Offersection() {
-	return <div>Offer</div>;
-}
-
-export default Offersection;
