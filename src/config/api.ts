5b12ab34f3eb9f0045e174ddea4dc4cd181ba858
shahin <shahinrazz345@gmail.com>
2024-01-03 17:18:36 +0530

    update

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 const API = {
-  // BASE_URL: "https://piccocabs-server-46642b82a774.herokuapp.com",
-  BASE_URL: "http://localhost:8080",
+  BASE_URL: "https://piccocabs-server-46642b82a774.herokuapp.com",
+  // BASE_URL: "http://localhost:8080",
 
   // GET_ALL_BOOKINGS: "/Booking/booking",
   GET_ALL_BOOKINGS: "/Booking/",
